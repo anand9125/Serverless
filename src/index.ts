b43d45049ea81b4e4dbb1031565201088b1a4091
Anand <achaudharyskn@gmail.com>
2024-09-26 00:30:12 +0530

    wrangler ugly

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,31 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+	export interface Env{
+		
+}
+//app.get("/user") what is worry about when we write express code 
+//body,header,query parameter..
+//this request function here handle everythting
+//we dont have to worry about this app.post(),app.get()...... 
+//Promise<Respons> this is what fucntion return response object  in genrics ?
+//  letts we will get somthinn from datbase so we have to  using async function so it is not returning respone it will returning a promise which returning response 
+//this is why 
+//in package .json if we write name as another then my app then it will 
+//start new worker
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		console.log(request.body);
+		console.log(request.headers);
+		console.log(request.method);
+		
+		
+		if (request.method === "GET") {
+			return Response.json({
+				message: "fuck you"
+			});
+		} else {
+			return Response.json({
+				message: "you did not send a get request"
+			});
+		}
 	},
-} satisfies ExportedHandler<Env>;
+};
